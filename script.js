7499428d313b9719874ea5e767bf4328f3b11213
LLIPION_219 <oleksandr_zabor@ukr.net>
2025-09-26 20:56:24 +0300

    Update-Roiova

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
- function showContact() {
-      const contactInfo = document.getElementById("contact-info");
-      if (contactInfo.style.display === "block") {
-        contactInfo.style.display = "none";
-      } else {
-        contactInfo.style.display = "block";
-      }
-    }
+function showContact() {
+  const contactInfo = document.getElementById("contact-info");
+  contactInfo.style.display = (contactInfo.style.display === "block") ? "none" : "block";
+}

6a5f78f6e1b268c7ade629978bd8de55a6b6b0ea
LLIPION_219 <oleksandr_zabor@ukr.net>
2025-09-26 20:48:41 +0300

    Update-add-Roiova

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function showContact() {
-  const contactInfo = document.getElementById("contact-info");
-  if (contactInfo.style.display === "block") {
-    contactInfo.style.display = "none";
-  } else {
-    contactInfo.style.display = "block";
-  }
-}
+ function showContact() {
+      const contactInfo = document.getElementById("contact-info");
+      if (contactInfo.style.display === "block") {
+        contactInfo.style.display = "none";
+      } else {
+        contactInfo.style.display = "block";
+      }
+    }

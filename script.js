8eca9e4a4a683eb9a1c13060871fe0b69040e3ed
LLIPION219 <oleksandr_zabor@ukr.net>
2026-05-04 14:32:15 +0300

    Update-Meta-participants

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function showContact() {
-  const contactInfo = document.getElementById("contact-info");
-  contactInfo.style.display = (contactInfo.style.display === "block") ? "none" : "block";
-}
+      function showContact() {
+      const info = document.getElementById("contact-info");
+      info.style.display = (info.style.display === "block") ? "none" : "block";
+    }
+
+    function toggleMembers() {
+      const list = document.getElementById("members-list");
+      const arrow = document.getElementById("arrow-icon");
+      list.classList.toggle("open");
+      arrow.style.transform = list.classList.contains("open") ? "rotate(180deg)" : "rotate(0deg)";
+    }
